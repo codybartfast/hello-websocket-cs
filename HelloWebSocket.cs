d551fa3958f772e8d985bbe977ac45e18b219839
John Donnellan <john@codybartfast.com>
2021-02-04 15:37:31 +0000

    Initial Commit

---
+++ HelloWebSocket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWebSocket
+{
+    class Program
+    {
+        static Encoding encoding = UTF8Encoding.UTF8;
+
+        static async Task Main()
+        {
+            using (var webSocket = new ClientWebSocket())
+            {
+                var serverUri = new Uri("wss://echo.websocket.org/");
+                await webSocket.ConnectAsync(serverUri, CancellationToken.None);
+
+                var rcvBuff = new byte[32];
+                var rcvTask = webSocket.ReceiveAsync(rcvBuff, CancellationToken.None);
+
+                var sndText = "Hello, WebSocket!";
+                var sndBuff = encoding.GetBytes(sndText);
+                await webSocket.SendAsync(sndBuff, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                var rcvMessage = await rcvTask;
+                var rcvText = encoding.GetString(rcvBuff, 0, rcvMessage.Count);
+                Console.WriteLine($"Received Text: \"{rcvText}\".");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello WebSocket
+
+#### A Simple Example of a .Net WebSocket Client.
+
+A very simple example of using the WebSocket Client in .Net 5.  It sends a single message to the server at [echo.websocket.org](https://www.websocket.org/echo.html) and then prints the returned message.
+
+It doesn't deal with any exceptions or anything other than the basic '[happy path](https://en.wikipedia.org/wiki/Happy_path)'.
